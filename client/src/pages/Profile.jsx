9dfa2b48fa78729ac20f76ae298c37595939bc39
utkarsh-choudhary <utkarshchoudhary734@gmail.com>
2024-06-04 06:58:52 +0530

    created update user api route

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -46,9 +46,6 @@ export default function Profile() {
     );
   }
 
-
-
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
